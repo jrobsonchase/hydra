02dd61cd19285b484763f0721e5ad0564a6a0976
Josh Robson Chase <josh@robsonchase.com>
2024-01-15 13:43:23 -0500

    support flakes in gitlab status

--- src/lib/Hydra/Plugin/GitlabStatus.pm
+++ src/lib/Hydra/Plugin/GitlabStatus.pm
@@ -14,7 +14,7 @@ use List::Util qw(max);
 #     status updates should be POST'ed, i.e. the jobset has a git input
 #     "nixexprs": "https://gitlab.example.com/project/nixexprs", in which
 #     case "gitlab_status_repo" would be "nixexprs".
-#   - gitlab_project_id => ID of the project in Gitlab, i.e. in the above
+#   - (optional) gitlab_project_id => ID of the project in Gitlab, i.e. in the above
 #     case the ID in gitlab of "nixexprs"
 
 sub isEnabled {
@@ -58,15 +58,16 @@ sub common {
                 description => "Hydra build #" . $build->id . " of $jobName",
                 name => "Hydra " . $build->get_column('job'),
             });
-        while (my $eval = $evals->next) {
-            my $gitlabstatusInput = $eval->jobsetevalinputs->find({ name => "gitlab_status_repo" });
-            next unless defined $gitlabstatusInput && defined $gitlabstatusInput->value;
-            my $i = $eval->jobsetevalinputs->find({ name => $gitlabstatusInput->value, altnr => 0 });
-            next unless defined $i;
-            my $projectId = $eval->jobsetevalinputs->find({ name => "gitlab_project_id" })->value;
-            my $accessToken = $self->{config}->{gitlab_authorization}->{$projectId};
-            my $rev = $i->revision;
-            my $domain = URI->new($i->uri)->host;
+
+        my $sendStatus = sub {
+            my ($domain, $owner, $repo, $projectId, $rev) = @_;
+            my $accessToken;
+            if (defined $projectId) {
+                $accessToken = $self->{config}->{gitlab_authorization}->{$projectId};
+            } else {
+                $accessToken = $self->{config}->{gitlab_authorization}->{$owner};
+                $projectId = "$owner/$repo" =~ s/\//%2F/rg;
+            }
             my $url = "https://$domain/api/v4/projects/$projectId/statuses/$rev";
             print STDERR "GitlabStatus POSTing $state to $url\n";
             my $req = HTTP::Request->new('POST', $url);
@@ -75,6 +76,42 @@ sub common {
             $req->content($body);
             my $res = $ua->request($req);
             print STDERR $res->status_line, ": ", $res->decoded_content, "\n" unless $res->is_success;
+        };
+
+        while (my $eval = $evals->next) {
+            if (defined $eval->flake) {
+                my $fl = $eval->flake;
+                print STDERR "Flake is $fl\n";
+                if ($eval->flake =~ m!gitlab:([^/]+)/([^/]+)/([[:xdigit:]]{40})$!) {
+                    my $owner = $1;
+                    my $repo = $2;
+                    my $rev = $3;
+                    $sendStatus->("gitlab.com", $owner, $repo, undef, $rev);
+                } elsif ($eval->flake =~ m!git\+ssh://git\@([^/]+)/([^/]+)/([^?]+)\?.*rev=([[:xdigit:]]{40})$!) {
+                    my $domain = $1;
+                    my $owner = $2;
+                    my $repo = $3;
+                    my $rev = $4;
+                    $sendStatus->($domain, $owner, $repo, undef, $rev);
+                } else {
+                    print STDERR "Can't parse flake, skipping Gitlab status update\n";
+                }
+            } else {
+                my $projectId;
+                my $projectIdInput = $eval->jobsetevalinputs->find({ name => "gitlab_project_id" });
+                if (defined $projectIdInput) {
+                    $projectId = $projectIdInput->value;
+                }
+                my $gitlabstatusInput = $eval->jobsetevalinputs->find({ name => "gitlab_status_repo" });
+                next unless defined $gitlabstatusInput && defined $gitlabstatusInput->value;
+                my $i = $eval->jobsetevalinputs->find({ name => $gitlabstatusInput->value, altnr => 0 });
+                next unless defined $i;
+                my $rev = $i->revision;
+                my $domain = URI->new($i->uri)->host;
+                my $uri = $i->uri;
+                $uri =~ m![:/]([^/]+)/(.+?)(?:.git)?$!;
+                $sendStatus->($domain, $1, $2, $projectId, $rev);
+            }
         }
     }
 }
